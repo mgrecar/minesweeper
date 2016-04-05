872c6c92c79b570b01c184aedec9b09179190e5c
Matthew Grecar <mgrecar@gmail.com>
2016-04-04 21:14:50 -0400

    Tween the grid tiles into place.
    Add a little flair with a slightly delayed tween on each tile element,
    causing it to alpha into opacity over a few seconds.

--- src/level.js
+++ src/level.js
@@ -1,3 +1,5 @@
+import Phaser from 'phaser';
+
 var level = class Level {
   create() {
     this.tileGrid = [];
@@ -8,6 +10,8 @@ var level = class Level {
     for (let x = 0; x < rows; x++) {
       for (let y = 0; y < columns; y++) {
         var gridTile = this.add.sprite(5 + x * gridCellWidth, 5 + y * gridCellHeight, 'mine');
+        gridTile.alpha = 0;
+        this.add.tween(gridTile).to( { alpha: 1 }, 2000, Phaser.Easing.Exponential.Out, true, x * 50 + y * 25, 0, false);
         this.tileGrid.push(gridTile);
       }
     }
