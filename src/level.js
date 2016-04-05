0fbebf96f84f59733fd1073da8b451de2e67396f
Matthew Grecar <mgrecar@gmail.com>
2016-04-04 20:31:01 -0400

    Create a grid of tiles in a level.
    A first start, we just create a 5 x 5 set of icons representing cells to
    click.  Space them out evenly and throw them into an array to reference.

--- src/level.js
+++ src/level.js
@@ -1,6 +1,16 @@
 var level = class Level {
   create() {
-    console.log('beginning Level');
+    this.tileGrid = [];
+    var rows = 5;
+    var columns = 5;
+    var gridCellWidth = 30;
+    var gridCellHeight = 30;
+    for (let x = 0; x < rows; x++) {
+      for (let y = 0; y < columns; y++) {
+        var gridTile = this.add.sprite(5 + x * gridCellWidth, 5 + y * gridCellHeight, 'mine');
+        this.tileGrid.push(gridTile);
+      }
+    }
   }
 };
 
